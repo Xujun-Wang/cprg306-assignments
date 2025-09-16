91a966b48e992a6961a83d7a37e61e058033d49e
Xujun-Wang <xujun.wang@edu.sait.ca>
2025-09-16 16:58:24 -0600

    updated root page

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,14 +4,16 @@ import Link from "next/link";
 function StudentInfo() {
   return (
     <div>
-      <h2>Xujun Wang</h2>
+      <h2>Name: Xujun Wang</h2>
       <p>
+        GitHub:&nbsp;
         <Link
           href="https://github.com/Xujun-Wang/cprg306-assignments"
           target="_blank"
+          className="underline"
           rel="noopener noreferrer"
         >
-          GitHub Repository
+          Xujun-Wang/cprg306-assignments
         </Link>
       </p>
     </div>
