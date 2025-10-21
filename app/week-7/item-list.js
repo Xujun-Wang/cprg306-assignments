f4d24ad4c7f6e3fe22dd0381a3826322ec6c2c25
Xujun-Wang <xujun.wang@edu.sait.ca>
2025-10-21 15:07:20 -0600

    initiated week 7 folder

---
+++ app/week-7/item-list.js
@@ -0,0 +1,58 @@
+"use client";
+
+import React from "react";
+import Item from "./item";
+import items from "./items.json";
+import { useState } from "react";
+
+export default function ItemList() {
+  // set default sort by name
+  const [sortBy, setSortBy] = useState("name");
+
+  // sort items based on sortBy state
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === "category") {
+      return a.category.localeCompare(b.category);
+    }
+    return 0;
+  });
+
+  return (
+    <section className="space-y-4">
+    <div className="flex items-center gap-2">
+      <span className="text-md text-white">Sort by:</span>
+      {/* Sort buttons */}
+        <button
+          onClick={() => setSortBy("name")}
+          className={`mr-2 px-4 py-2 rounded ${
+            sortBy === "name" ? "bg-pink-500 text-white font-bold" : "bg-gray-500 text-white text-sm"
+          }`}
+        >
+          Name
+        </button>
+        <button
+          onClick={() => setSortBy("category")}
+          className={`px-4 py-2 rounded ${
+            sortBy === "category" ? "bg-pink-500 text-white font-bold" : "bg-gray-500 text-white text-sm"
+          }`}
+        >
+          Category
+        </button>
+      </div>
+
+      {/* Items list */}
+      <ul className="space-y-2">
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            category={item.category}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+}
