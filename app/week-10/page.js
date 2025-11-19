c8c4e3f1ad78eae7b9abc12b965b079dd8c69ccb
Xujun-Wang <xujun.wang@edu.sait.ca>
2025-11-18 21:52:29 -0700

    solved typo issues

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -33,7 +33,7 @@ export default function Page() {
           </p>
           <div className="space-x-4">
             <Link
-              href="/week-10/shopping-lsit"
+              href="/week-10/shopping-list"
               className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
             >
               Go to Shopping List
